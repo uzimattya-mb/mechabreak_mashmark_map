1b1e46852b1b3a8fa7bc16823900f49756b592fa
uzimattya-mb <lieclown@yahoo.co.jp>
2026-04-04 01:54:18 +0900

    Add base-map GBmarker for now

--- js/map.js
+++ js/map.js
@@ -25,27 +25,27 @@
 // ];
 
 const markers = [
-    { id: 1, x: 16, y: 20, title: "ダストロック基地", image: "images/details/detail-1.jpg" },
-    { id: 2, x: 23, y: 14, title: "インパクト・ゾーン", image: "images/details/detail-2.jpg" },
+    { id: 1, x: 14, y: 18, title: "ダストロック基地", image: "images/details/detail-1.jpg" },
+    { id: 2, x: 19, y: 11, title: "インパクト・ゾーン", image: "images/details/detail-2.jpg" },
     { id: 3, x: 28, y: 28, title: "サンドダスト庇護所（まだ画像なし）", image: "images/details/detail-3.jpg" },
-    { id: 4, x: 12, y: 41, title: "太陽の岩", image: "images/details/detail-4.jpg" },
+    { id: 4, x: 10, y: 40, title: "太陽の岩", image: "images/details/detail-4.jpg" },
     { id: 5, x: 2, y: 55, title: "石湾基地", image: "images/details/detail-5.jpg" },
-    { id: 6, x: 14, y: 59, title: "星の岩礁外", image: "images/details/detail-6.jpg" },
-    { id: 7, x: 18, y: 67, title: "星の岩礁室内",image: "images/details/detail-7.jpg" },
-    { id: 8, x: 25, y: 77, title: "廃工場", image: "images/details/detail-8.jpg" },
-    { id: 9, x: 51, y: 82, title: "サウスウッド", image: "images/details/detail-9.jpg" },
-    { id: 10, x: 73, y: 88, title: "南投空港", image: "images/details/detail-10.jpg" },
+    { id: 6, x: 12, y: 58, title: "星の岩礁外", image: "images/details/detail-6.jpg" },
+    { id: 7, x: 16, y: 65, title: "星の岩礁室内",image: "images/details/detail-7.jpg" },
+    { id: 8, x: 23, y: 77, title: "廃工場", image: "images/details/detail-8.jpg" },
+    { id: 9, x: 51, y: 83, title: "サウスウッド", image: "images/details/detail-9.jpg" },
+    { id: 10, x: 74, y: 88, title: "南投空港", image: "images/details/detail-10.jpg" },
     { id: 11, x: 83, y: 86, title: "芯石精錬所（まだ画像なし）", image: "images/details/detail-11.jpg" },
-    { id: 12, x: 90, y: 69, title: "鉄錆の波止場", image: "images/details/detail-12.jpg" },
-    { id: 13, x: 89, y: 39, title: "日の出海岸", image: "images/details/detail-13.jpg" },
-    { id: 14, x: 92, y: 12, title: "飛び坂の島", image: "images/details/detail-14.jpg" },
-    { id: 15, x: 85, y: 24, title: "中継前哨基地（まだ画像なし）", image: "images/details/detail-15.jpg" },
-    { id: 16, x: 79, y: 24, title: "中継前哨基地室内", image: "images/details/detail-16.jpg" },
-    { id: 17, x: 78, y: 19, title: "中継前哨基地北側", image: "images/details/detail-17.jpg" },
-    { id: 18, x: 71, y: 27, title: "中継前哨基地西側（まだ画像なし）", image: "images/details/detail-18.jpg" },
-    { id: 19, x: 62, y: 16, title: "森の要塞東側（まだ画像なし）", image: "images/details/detail-19.jpg" },
-    { id: 20, x: 57, y: 16, title: "森の要塞西側", image: "images/details/detail-20.jpg" },
-    { id: 21, x: 53, y: 54, title: "巨大カルデラ（まだ画像なし）", image: "images/details/detail-21.jpg" }
+    { id: 12, x: 92, y: 69, title: "鉄錆の波止場", image: "images/details/detail-12.jpg" },
+    { id: 13, x: 91, y: 39, title: "日の出海岸", image: "images/details/detail-13.jpg" },
+    { id: 14, x: 94, y: 9, title: "飛び坂の島", image: "images/details/detail-14.jpg" },
+    { id: 15, x: 86, y: 23, title: "中継前哨基地（まだ画像なし）", image: "images/details/detail-15.jpg" },
+    { id: 16, x: 80, y: 22, title: "中継前哨基地室内", image: "images/details/detail-16.jpg" },
+    { id: 17, x: 79, y: 17, title: "中継前哨基地北側", image: "images/details/detail-17.jpg" },
+    { id: 18, x: 71, y: 25, title: "中継前哨基地西側（まだ画像なし）", image: "images/details/detail-18.jpg" },
+    { id: 19, x: 62, y: 14, title: "森の要塞東側（まだ画像なし）", image: "images/details/detail-19.jpg" },
+    { id: 20, x: 57, y: 14, title: "森の要塞西側", image: "images/details/detail-20.jpg" },
+    { id: 21, x: 53, y: 53, title: "巨大カルデラ（まだ画像なし）", image: "images/details/detail-21.jpg" }
     // { id: 22, x: 50, y: 40, title: "GoldenBox_22", image: "images/details/detail-22.jpg" }
 ];
 
